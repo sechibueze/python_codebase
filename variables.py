ac19b81e0601d8f8877f07cc8a011c230ee0a3e6
Samuel Chibueze <sechibueze@gmail.com>
2022-09-01 09:56:56 +0100

    variables playbook

--- variables.py
+++ variables.py
@@ -1,20 +1,20 @@
-# from imghdr import what
-# from tkinter import Variable
-
-
+# Introduction to Variables
 # what is a Variable?
-
 # Declare and use a Variable
-from tkinter import Variable
+# Variable naming conventions
 
-
+# Varibales are used to store data
 name = "Samuel"
 age = 23
+is_hungry = False
+price = 99.07
 print(name)
 print(age)
+# TODO - display the value of the other two variable
+
 
 # Over-riding varibales
-age = 2
+age = 32
 print(age)
 
 # Rules for naming variables
@@ -23,15 +23,30 @@ print(age)
 # - Python keywords cannot be used as variable name. Example: print, for, not, is, while, def
 # Varible names are case-sensitive ie: firstname is not same as firstName
 
-first_name = ""
+first_name = "Kingsley"
 _counter = 0
-lastName = 0
+lastName = "Olatunji"
+LastName = "Michael"
+# TODO - display each of the values
+
+# Varible naming convention - 
+# - Camel casing: firstName
+# - Snake casing: first_name
+# - pascal case: FirstName
+# Further reading: https://khalilstemmler.com/blogs/camel-case-snake-case-pascal-case/
+
+# Practice: Using snake_case convention, Declare menaingful variables to store the following
+# - the name of customer
+# - the name of customer
+# - the address of customer
+# - the occupation of customer
+# - the date of birth of customer
+# - the customer's current balance
+# - the customer's telephone number
+
+# Supplementary reading
 # Recommended practice for naming variables @PEP guide
+# https://peps.python.org/pep-0008/#introduction
+#  https://realpython.com/python-pep8/
 
-# Varible naming convention
-# - Camel casing
-# - Snake casing
-# - pascal case
-
-# Practice
 
